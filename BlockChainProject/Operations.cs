95928e2051a394bb7e8536096b3fcd9d8d0159cb
Matisimo <matas.remeskevicius@gmail.com>
2020-09-28 18:32:27 +0300

    Adding functions to Operations.cs

---
+++ BlockChainProject/Operations.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlockChainProject
+{
+    class Operations
+    {
+        string sigma0(string a) //ROTR 7, ROTR18, SHR3, XOR 2-3, XOR 1-2
+        {
+
+        }
+
+        string sigma1(string a) //ROTR 17, ROTR 19, SHR10
+        {
+
+        }
+
+        string usigma0(string a) //ROTR2, ROTR 13, ROTR22
+        {
+
+        }
+
+        string usigma1(string a, string b) //ROTR6, ROTR11, ROTR25
+        {
+
+        }
+
+        string choice(string a, string b, string c) //use the first input, to determine, whether to use the second or the third, if its a 1, it takes string b, if its 0, it takes string c
+        {
+
+        }
+        string majority(string a, string b) //it takes the majority of bits
+        {
+
+        }
+
+    }
+}
